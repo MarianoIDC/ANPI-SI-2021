57e72dd5c494fac1f041d4a5673f504c1e73ac2d
Luis Daniel Prieto <prieto.luisdaniel@gmail.com>
2021-03-17 20:49:53 -0600

    Se implementa el método de falsa posición y las gráficas

--- Catalogo_Grupo_5/CodigosC++/FalsaPosicion.cpp
+++ Catalogo_Grupo_5/CodigosC++/FalsaPosicion.cpp
@@ -1,64 +1,111 @@
 #include <iostream>
 #include <ginac/ginac.h>
-#include <mgl2/mgl.h>
-#include <cmath>
+#include "mgl2/mgl.h"
+#include <vector>
 
 using namespace std;
 using namespace GiNaC;
 
+/*Función para crear una gráfica:
+ * Entradas: Pares ordenados en x y y, vectores de las gráficas
+ * Salidas: Gráfica de iteraciones vs error*/
+void createGraph(double x1, double x2, double y1, double y2, vector<double> x, vector<double> y) {
+    mglGraph graph;
+    //Estas funciones convierten los vectores de la entrada en arreglos de datos de la gráfica
+    mglData xGraph(x);
+    mglData yGraph(y);
+    //Se diseña la gráfica con los parámetros
+    graph.Title("Error vs Iteración");
+    graph.SetOrigin(0, 0);
+    graph.SetRanges(x1, x2, y1, y2); //Límites de la gráfica
+    graph.Plot(xGraph, yGraph, "o!rgb"); //Valores que va a contener la gráfica
+    graph.Axis();
+    graph.Grid();
+    //Se exporta la gráfica a un archivo PNG
+    graph.WritePNG("Graph.png");
+}
+
+/*Método de la falsa posición:
+ * Entradas: Función a la que se le va a aplicar el método (express), primer valor inicial, segundo
+   valor inicial, tolerancia y cantidad de iteraciones máximas
+ * Salidas: Aproximación de la solución, error y cantidad de iteraciones realizadas*/
 ex falsaPosicion (string express, string firstValue, string secondValue, string tolerance, string iterations) {
+    //Implementación del cálculo simbólico
     symbol x("x");
     symtab table;
     table["x"] = x;
     parser reader(table);
+    //Se traducen las entradas a variables de cálculo simbólico
     ex function = reader(express);
     ex x0 = reader(firstValue);
     ex x1 = reader(secondValue);
     ex tol = reader(tolerance);
     ex iterMax = reader(iterations);
+    //Se definen las variables de la iteración, solución y error necesarias
     int iter = 1;
     ex xk;
-    ex error;
-    while (iter < iterMax) {
+    ex error = tol + 1;
+    //Vectores para la gráfica
+    vector<double> errors;
+    vector<double> iters;
+    while (iter < iterMax && error <= tol) {
+        //Funciones por evaluar
         ex f0 = evalf(subs(function, x == x0));
         ex f1 = evalf(subs(function, x == x1));
         ex fx = evalf(subs(function, x == xk));
+        //Comprueba el Teorema de Bolzano
         if (f1 * f0 > 0) {
             cout << "No se cumple el Teorema de Bolzano" << endl;
             return 0;
         }
-        else if (error < tol) {
-            return xk;
-        }
+        //Comprueba el segundo intervalo
         else if (f1 * fx < 0) {
             x1 = xk;
         }
+        //Comprueba el primer intervalo
         else if (f0 * fx < 0) {
             x0 = xk;
         } else {
+            //Ecuación del método de la falsa posición
             xk = x1 - f1 * ((x1 - x0) / f1 - f0);
             error = abs(fx);
+            ex aux = evalf(error);
             iter++;
+            //Los vectores de iteración y error reciben valores
+            double m = ex_to<numeric>(aux).to_double();
+            errors.push_back(m);
+            iters.push_back(iter);
         }
     }
+    cout << "Aproximación: " << xk << endl;
+    cout << "Iteraciones : " << iter << endl;
+    cout << "Error : " << error << endl;
+    //Se crea la gráfica respectiva
+    createGraph(0, iter + 1, -ex_to<numeric>(evalf(error)).to_double(), ex_to<numeric>(evalf(error)).to_double(), iters, errors);
+    return xk;
 }
 
 int main() {
+    //Se define la función por evaluar
     string express;
     cout << "Escriba la función: " << endl;
     cin >> express;
+    //Se definen los intervalos iniciales
     string x0;
     cout << "Escriba el primer valor inicial: " << endl;
     cin >> x0;
     string x1;
     cout << "Escriba el segundo valor inicial: " << endl;
     cin >> x1;
+    //Se define la tolerancia
     string tol;
     cout << "Escriba la tolerancia: " << endl;
     cin >> tol;
+    //Se define el número máximo de iteraciones
     string iterMax;
     cout << "Escriba el número de iteraciones: " << endl;
     cin >> iterMax;
+    //Método de la falsa posición
     falsaPosicion(express, x0, x1, tol, iterMax);
     return 0;
 }
