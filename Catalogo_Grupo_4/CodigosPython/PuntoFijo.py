39e07caa6d22bcc812b7915d63ee25a14c2f73d5
Jonathana1196 <jonathana1196@gmail.com>
2021-03-26 17:15:16 -0600

    Modificaciones Catalogo

--- Catalogo_Grupo_4/CodigosPython/PuntoFijo.py
+++ Catalogo_Grupo_4/CodigosPython/PuntoFijo.py
@@ -8,29 +8,29 @@ import numpy as np
 #
 #
 
-def punto_fijo(funcion,valor_inicial,iteraciones_maximas):
+def punto_fijo(funcion, valor_inicial, iteraciones_maximas):
     lista_error = [] #lista para graficar 
     iteracion = 1 
     b = funcion(valor_inicial)  #valor para obtener error
     error = abs(b-valor_inicial)
-    while(iteracion<=iteraciones_maximas ):  #condicion de parada
+    while(iteracion <= iteraciones_maximas ):  #condicion de parada
         valor_inicial = b                    #reajuste de valores de error
         b = funcion(valor_inicial)
-        error = abs(b-valor_inicial)
+        error = abs(b - valor_inicial)
         lista_error.append(error)
         iteracion += 1
     
     aproximacion = b
-    plt.plot(lista_error, label='errores por interacion')    #Construccion de tabla
+    plt.plot(lista_error, label = 'errores por interacion') #Construccion de tabla
     plt.ylabel('Error')
     plt.xlabel('Iteracion')
     
-    plt.axis([0, iteraciones_maximas,0,lista_error[0]])      #Los ejes estan limitados por las iteraciones y el error maximo
+    plt.axis([0, iteraciones_maximas, 0, lista_error[0]]) #Los ejes estan limitados por las iteraciones y el error maximo
     plt.title('Punto Fijo')
     plt.legend()
     plt.show()
-    print ('Aproximacion: '+ str(aproximacion)+ ', error: '+ str(error))
+    print('Aproximacion: '+ str(aproximacion)+ ', error: '+ str(error))
     return aproximacion, error
 
-funcion =lambda x: np.exp(-x) 
+funcion = lambda x: np.exp(-x) 
 punto_fijo(funcion, 0, 15)
