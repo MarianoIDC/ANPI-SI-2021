baacc9f977eb57e95a6702dcd56a59f0fb261378
Jonathan G Araya <jonathana1196@gmail.com>
2021-04-28 14:48:36 -0600

    Eliminacion Gaussiana y Factorizacion LU
    Completos

---
+++ Catalogo_Grupo_4/CodigosOctave/EliminacionGaussiana.m
@@ -0,0 +1,79 @@
+%{
+    Metodo de Eliminacion Gaussiana
+    Parametros de Entrada
+        @param matrizD: matriz de coeficientes
+        @param matrizI: vector de terminos independientes
+    
+    Parametros de Salida
+        @return vectorResultado: solucion del sistema
+%}
+
+clc;
+clear;
+pkg load symbolic;
+format long;
+warning('off', 'all');
+
+function X = gaussiana(matrizD, matrizI)
+  
+  [n, m] = size(matrizD);
+  if (n ~= m)
+    disp("La matrizD debe ser cuadrada");
+  end
+
+  n = length(matrizD);
+  X = [matrizD, matrizI];
+  % Por cada argumento de la matriz
+  for i=1:n
+    pivot = X(i, i);
+    pivotRow = X(i, :);
+    % Multiplica los vectores
+    M = zeros(1, n - i);
+    m = length(M);
+    % Obtiene cada fila multiplicada
+    for k=1:m
+      M(k) = X(i + k, i) / pivot;
+    endfor
+    % Modifica cada fila
+    for k=1:m
+      X(i + k, :) = X(i + k, :) - pivotRow*M(k);
+    endfor
+  endfor
+  X = sustitucionAtras(X(1:n, 1:n), X(:,n+1));
+endfunction
+
+%{
+    Metodo de Sustitucion Atras
+    -Resuelve un sistema del tipo Ax = b
+    Parametros de Entrada
+        @param matrizA: matriz triangular superior NxN
+        @param matrizB: matriz Nx1
+    
+    Parametros de Salida
+        @return X: solucion de la matriz
+%}
+
+function X = sustitucionAtras(matrizA, matrizB)
+    n = length(matrizB);
+    X = zeros(n, 1);
+    X(n) = matrizB(n)/matrizA(n, n);
+    
+    for(k = n-1 : -1 : 1)
+        div = matrizA(k, k);
+    if (div != 0)
+        X(k) = (matrizB(k) - matrizA(k, k+1:n)*X(k+1:n))/matrizA(k, k);
+    else
+        disp("Error: se ha producido una division por cero");
+    endif
+  endfor
+endfunction
+
+%Matriz de coeficientes
+A = [2 -6 12 16 ; 1 -2 6 6; -1 3 -3 -7; 0 4 3 -6];
+%Matriz de terminos independientes
+B = [70 26 -30 -26]';
+%Llamado de la funcion
+X = gaussiana(A, B);
+printf("############################################ \n");
+printf("Metodo de la Eliminacion Gaussiana \n");
+printf('X = %f\n', X);
---
+++ Catalogo_Grupo_4/CodigosPython/FactorizacionLU.py
@@ -0,0 +1,50 @@
+# Metodo de la Factorizacion LU
+    # Entradas:
+        # matrizD: matriz de coeficientes
+        # matrizI: matriz de terminos independientes
+    # Salidas:
+        # X: solucion del sistema
+
+###############################################################################
+import numpy as np
+###############################################################################
+
+def fact_lu(matrizD, matrizI):
+    n = len(matrizD)
+    L = np.eye(n)
+    U = matrizD
+
+    for i in range(1, n):
+        pivot = U[i - 1][i - 1]
+        pivotRow = U[i - 1]
+        M = np.zeros((1, n - i))
+        m = M.size + 1
+
+        for k in range(1, m):
+            try:
+                M[i - 1][k - 1] = (U[i + k - 1][i - 1]) / pivot
+            except:
+                M = (U[i + k - 1][i - 1]) / pivot
+
+        for k in range(1, m):
+            try:
+                U[i + k - 1] = U[i + k - 1] - (np.multiply(pivotRow, M[i - 1][k - 1]))
+                L[i + k - 1][i - 1] = M[i - 1][k - 1]
+            except:
+                U[i + k - 1] = U[i + k - 1] - (np.multiply(pivotRow, M))
+                L[i + k - 1][i - 1] = M
+
+    Y = ((np.linalg.inv(L)).dot(np.transpose(matrizI)))
+    X = (np.linalg.inv(U)).dot(Y)
+    return X
+
+if __name__ == '__main__':
+    # Matriz de coeficientes
+    A = [[4, -2, 1], [20, -7, 12], [-8, 13, 17]]
+    # Vector de terminos independientes
+    B = [11, 70, 17]
+    # Llamado de la funcion
+    X = fact_lu(A, B)
+    print("######################################################")
+    print("Metodo de la Factorizacion LU\n")
+    print('X = {}\n'.format(X))
