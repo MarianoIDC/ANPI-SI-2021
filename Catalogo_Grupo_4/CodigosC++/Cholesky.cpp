3627cda482b445ab6cecd73ee49ea4893409e74c
Luis Daniel Prieto <prieto.luisdaniel@gmail.com>
2021-04-30 22:56:04 -0600

    Cholesky funciona por completo

--- Catalogo_Grupo_4/CodigosC++/Cholesky.cpp
+++ Catalogo_Grupo_4/CodigosC++/Cholesky.cpp
@@ -12,18 +12,17 @@ mat cholesky(mat A) {
     mat L(A.n_rows, A.n_cols, fill::zeros);
     for (int i = 0; i < A.n_rows; i++) {
         for (int j = 0; j < i + 1; j++) {
-            int suma1 = 0;
-            int suma2 = 0;
+            double suma = 0;
             if (j == i) {
                 for (int k = 0; k < j; k++) {
-                    suma1 += pow((double)L(j, k), 2.0);
-                L(j, j) = sqrt(A(j, j) - suma1);
+                    suma += L(j, k) * L(j, k);
                 }
+                L(j, j) = sqrt(A(j, j) - suma);
             } else {
                 for (int k = 0; k < j; k++) {
-                    suma2 += L(i, k) * L(j, k);
-                L(i, j) = (A(i, j) - suma2)/L(j, j);
+                    suma += L(i, k) * L(j, k);
                 }
+                L(i, j) = (A(i, j) - suma)/L(j, j);
             }
         }
     }
@@ -36,16 +35,15 @@ mat cholesky(mat A) {
 // Un vector d que es el vector de términos independientes
 //Salidas: Un vector y que es la solución de este sistema de ecuaciones
 colvec sust_atras(mat L, colvec y) {
-    int n = L.n_rows;
     colvec x(L.n_rows, fill::zeros);
-    for (int i = n - 1; i > 0; i--) {
+    for (int i = L.n_rows - 1; i > -1; i--) {
         int suma = 0;
-        for (int j = i + 1; j < n; j++) {
+        for (int j = i; j < L.n_rows; j++) {
             suma += L(i, j) * x(j);
         }
         x(i) = (y(i) - suma)/L(i, i);
     }
-    return y;
+    return x;
 }
 
 //Función que realiza la sustitución hacia atrás
@@ -53,15 +51,14 @@ colvec sust_atras(mat L, colvec y) {
 // Una matriz L que es la transpuesta de la factorización de Cholesky de otra matriz
 // Un vector y que es el vector de términos independientes
 //Salidas: Un vector x que es la solución de este sistema de ecuaciones
-colvec sust_adelante(mat L, colvec d) {
-    int n = L.n_rows;
+colvec sust_adelante(mat L, colvec b) {
     colvec y(L.n_rows, fill::zeros);
-    for (int i = n - 1; i > 0; i--) {
-        int suma = 0;
-        for (int j = 1; j < i - 1; j++) {
+    for (int i = 0; i < L.n_rows; i++) {
+        double suma = 0;
+        for (int j = 0; j < i; j++) {
             suma += L(i, j) * y(j);
         }
-        y(i) = (d(i) - suma)/L(i, i);
+        y(i) = (b(i) - suma)/L(i, i);
     }
     return y;
 }
@@ -71,20 +68,21 @@ colvec sust_adelante(mat L, colvec d) {
 // Una matriz A de cualquier tamaño
 // Un vector d que es el vector de términos independientes
 //Salidas: Un vector x que es la solución del sistema de ecuaciones
-colvec fact_Cholesky(mat A, colvec b) {
+void fact_Cholesky(mat A, colvec b) {
     //Revisa si es simétrica positiva definida con una función propia de Armadillo
     if (!A.is_sympd()) {
         A = A * trans(A);
         b = b * trans(A);
-    } else {
-        A = A;
-        b = b;
     }
     //Llama a las demás funciones y las guarda en variables
+    cout << "Matriz: \n" << A << endl;
+    cout << "Vector: \n" << b << endl;
     mat L = cholesky(A);
+    cout << "Matriz factorizada: \n" << L << endl;
     colvec y = sust_adelante(L, b);
+    cout << "Vector independiente: \n" << y << endl;
     colvec x = sust_atras(trans(L), y);
-    return x;
+    cout << "Solución del sistema: \n" << x << endl;
 }
 
 int main() {
@@ -93,7 +91,6 @@ int main() {
     //Vector d de términos independientes
     colvec d = "-25 -19 -21 -5";
     //Realiza la factorización de Cholesky
-    colvec testCholesky = fact_Cholesky(A, d);
-    cout << "Solución del sistema: \n" << testCholesky << endl;
+    fact_Cholesky(A, d);
     return 0;
 }
