a2fb8438429ffb1a1ddcacc7a30e264963e8fb12
MarianoIDC <mmunoz@estudiantec.cr>
2021-05-07 21:43:19 -0600

    update

--- Tarea 2 - Grupo 4/Parte 2/parte2_p4.py
+++ Tarea 2 - Grupo 4/Parte 2/parte2_p4.py
@@ -3,7 +3,6 @@ import numpy as np
 
 from parte2_p2 import *
 
-
 x = np.array(['x1','x2','x3','x4'], dtype = object)
 x0 = np.array([1,-1,1,-1], dtype = float)
 f = np.array([
@@ -13,8 +12,17 @@ f = np.array([
     '(0.2*(tan(x1)+tan(x2)+tan(x3)+tan(x4))-2)'
     ], 
     dtype = object)
+"""
 
-
+x = np.array(['x1','x2','x3'], dtype = object)
+x0 = np.array([0.7,0.2,0.1], dtype = float)
+f = np.array([
+    '98066.5*((x1+x2)*(x1-x3))-(5.97*(1-x1-x2)*(1+x1+x2))',
+    '98066.5*((x2+x3)*x2)-(0.27*(1+x1+x2)*(1+x1+x2))',
+    '((x1+x3)*x3)-(2.8*(x1-x3)*(x2-x3))'
+    ], 
+    dtype = object)
+"""
 tol = 0.0001
 iterMax = 10
 print("Método de Newton-Raphson para el problema ingenieril \n")
