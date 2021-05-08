3ba72409239a98adfaca9aa5b6cfff68b85db02c
Java-Sith <prieto.luisdaniel@gmail.com>
2021-05-07 18:24:57 -0600

    Se corrige la prueba

--- Tarea 2 - Grupo 4/Parte 2/parte2_p3.py
+++ Tarea 2 - Grupo 4/Parte 2/parte2_p3.py
@@ -30,16 +30,55 @@ def funcionE(x0, iterMax, tol):
     xk, itera, error, listaIter, listaError = newton_raphson(x, f, x0, tol, iterMax)
     return xk, itera, error, listaIter, listaError
 
-def funcionD(x0, iterMax, tol):
+def funcionG(x0, iterMax, tol):
     x = np.array(['x1', 'x2'], dtype = object)
     f = np.array(['exp(x1**2) - exp(sqrt(2)*x1)', 'x1 - x2'], dtype = object)
     xk, itera, error, listaIter, listaError = newton_raphson(x, f, x0, tol, iterMax)
     return xk, itera, error, listaIter, listaError
 
-select = int(input('Escoja un número del 1 al 5: '))
-x0 = np.array([0.8, 0.8])
-iterMax = 10
-tol = 0.0001
-xAprox, k, err, iter1, err1 = funcionA(x0, iterMax, tol)
-print('xAprox = {}\n%Error = {}\n%Iteraciones = {}'.format(xAprox, err, k))
-grafica(iter1, err1)
+select = int(input('Escoja un número del 1 al 6: '))
+if (select == 1):
+    x0 = np.array([2.3, 2.3])
+    iterMax = 10
+    tol = 0.0001
+    xAprox, k, err, iter1, err1 = funcionA(x0, iterMax, tol)
+    print('xAprox = {}\n%Error = {}\n%Iteraciones = {}'.format(xAprox, err, k))
+    grafica(iter1, err1)
+elif (select == 2):
+    x0 = np.array([1.5, 2])
+    iterMax = 10
+    tol = 0.0001
+    xAprox, k, err, iter1, err1 = funcionB(x0, iterMax, tol)
+    print('xAprox = {}\n%Error = {}\n%Iteraciones = {}'.format(xAprox, err, k))
+    grafica(iter1, err1)
+elif (select == 3):
+    x0 = np.array([3, 2])
+    iterMax = 10
+    tol = 0.0001
+    xAprox, k, err, iter1, err1 = funcionC(x0, iterMax, tol)
+    print('xAprox = {}\n%Error = {}\n%Iteraciones = {}'.format(xAprox, err, k))
+    grafica(iter1, err1)
+elif (select == 4):
+    x0 = np.array([-1, -2])
+    iterMax = 10
+    tol = 0.0001
+    xAprox, k, err, iter1, err1 = funcionD(x0, iterMax, tol)
+    print('xAprox = {}\n%Error = {}\n%Iteraciones = {}'.format(xAprox, err, k))
+    grafica(iter1, err1)
+elif (select == 5):
+    x0 = np.array([1.2, -1.5])
+    iterMax = 10
+    tol = 0.0001
+    xAprox, k, err, iter1, err1 = funcionB(x0, iterMax, tol)
+    print('xAprox = {}\n%Error = {}\n%Iteraciones = {}'.format(xAprox, err, k))
+    grafica(iter1, err1)
+elif (select == 6):
+    x0 = np.array([2, 2, 2, 0])
+    iterMax = 10
+    tol = 0.0001
+    xAprox, k, err, iter1, err1 = funcionB(x0, iterMax, tol)
+    print('xAprox = {}\n%Error = {}\n%Iteraciones = {}'.format(xAprox, err, k))
+    grafica(iter1, err1)
+else:
+    print('Por favor digite un número correcto')
+    return
