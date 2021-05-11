68842157f04ec8f91895ca6d4b4fab35510887a0
Jonathan G Araya <jonathana1196@gmail.com>
2021-05-10 18:31:04 -0600

    LaTeX
    Jacobi version python, trazador cubico y diferencias divididas

---
+++ Catalogo_Grupo_4/CodigosPython/Jacobi.py
@@ -0,0 +1,32 @@
+import numpy as np
+def jacobi(A, b, x0, tol):
+    n = len(A)
+    A = np.array(A)
+    b = np.array(b)
+    L = np.zeros((n, n))
+    D = np.zeros((n, n))
+    U = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(n):
+            if i > j:
+                L[i][j] = A[i][j]
+            elif i < j:
+                U[i][j] = A[i][j]
+            else:
+                D[i][j] = A[i][j]
+
+    x = np.array(x0)
+    error = np.linalg.norm(np.dot(A, x) - b)
+    i = 0
+
+    m_inv = np.linalg.inv(D)
+    m_times_n = np.dot(m_inv, (-1 * (L + U)))
+    m_times_b = np.dot(m_inv, b)
+
+    while error >= tol:
+        x = np.dot(m_times_n, x) + m_times_b
+        error = np.linalg.norm(np.dot(A, x) - b)
+        i += 1
+
+    return x
--- Catalogo_Grupo_4/CodigosPython/TrazadorCubicoNatural.py
+++ Catalogo_Grupo_4/CodigosPython/TrazadorCubicoNatural.py
@@ -4,12 +4,7 @@ import matplotlib.pyplot as plt
 import sympy
 import sympy as sym
 from sympy import symbols
-from sympy.plotting import plot
-
-from sympy import symbols
-from numpy import linspace
-from sympy import lambdify
-import matplotlib.pyplot as mpl
+from Jacobi import jacobi
 
 def trazador_cubico(func, S):
     # Procedemos a evaluar los puntos 'x'
@@ -26,20 +21,15 @@ def trazador_cubico(func, S):
         points.append([S[i], valoresY[i]])
 
     points = np.array([np.array(p) for p in points])
-    print("Los puntos son:\n", points, "\n")
     # Calculando los delta_hk
     delta_hk = points[1:, 0] - points[:-1, 0]
-    print("Los delta_hk son:\n", delta_hk, "\n")
     # Calculando los delta_yk
     delta_yk = points[1:, 1] - points[:-1, 1]
-    print("Los delta_yk son:\n", delta_yk, "\n")
     # La matriz y el vector para resolver el sistema
     A, u = [], []
-    print("A es:\n", A, "\n")
-    print("u es:\n", u, "\n")
     # Cantidad de puntos -1 (n)
     k = delta_hk.shape[0]
-    print("k es:\n", k, "\n")
+    
     for i in range(1, k):
         # Primer caso Ms[1] = 0
         if i == 1:
@@ -54,18 +44,11 @@ def trazador_cubico(func, S):
     # Convirtiendolo a numpy array
     A = np.array([np.array(a) for a in A])
     u = np.array(u)
-    print("A es:\n", A, "\n")
-    print("u es:\n", u, "\n")
     # Resolviendo el sistema mediante Thomas, LU o Jacobi
     x0 = np.zeros(u.shape)
-    # Ms = gauss_seidel(A, u, u*0, 0.000001)
-    # Ms = fact_lu(A, u)
     Ms = jacobi(A, u, u*0, 0.0000001)
-    print("X0 es:\n", x0, "\n")
-    print("Ms es:\n", Ms, "\n")
     # Append Ms[1] = 0 and Ms[n+1] = 0
     Ms = np.append(0, np.append(Ms, 0))
-    print("Ms es:\n", Ms, "\n")
     # Coeficientes
     a, b, c, d = [[], [], [], []]
     # Puntos iniciales
@@ -77,6 +60,7 @@ def trazador_cubico(func, S):
         b.append(Ms[i] / 2)
         c.append((yk[i + 1] - yk[i]) / delta_hk[i] - (2 * delta_hk[i] * Ms[i] + delta_hk[i] * Ms[i + 1]) / 6)
         d.append(yk[i])
+    
     # Convirtiendo
     a = np.array(a)
     b = np.array(b)
@@ -100,16 +84,16 @@ def trazador_cubico(func, S):
         px_tabla.append(pxtramo)
 
     # Polinomios por tramos
-    print('Polinomios por tramos: ')
-    for tramo in range(1, len(S)-1, 1):
-        print(' x = [' + str(S[tramo - 1]) + ',' + str(S[tramo]) + ']')
-        print(str(px_tabla[tramo - 1]))
+    # print('Polinomios por tramos: ')
+    # for tramo in range(1, len(S)-1, 1):
+    #     print(' x = [' + str(S[tramo - 1]) + ',' + str(S[tramo]) + ']')
+    #     print(str(px_tabla[tramo - 1]))
 
-    print("Sx0 es:\n", Sxi[0], "\n")
-    print("Sx1 es:\n", Sxi[1], "\n")
-    print("Sx2 es:\n", Sxi[2], "\n")
-    print("Sx3 es:\n", Sxi[3], "\n")
-    print("Sx4 es:\n", Sxi[4], "\n")
+    # print("Sx0 es:\n", Sxi[0], "\n")
+    # print("Sx1 es:\n", Sxi[1], "\n")
+    # print("Sx2 es:\n", Sxi[2], "\n")
+    # print("Sx3 es:\n", Sxi[3], "\n")
+    # print("Sx4 es:\n", Sxi[4], "\n")
 
     xtraza = np.array([])
     ytraza = np.array([])
@@ -130,115 +114,27 @@ def trazador_cubico(func, S):
         ytraza = np.concatenate((ytraza, ytramo))
         tramo = tramo + 1
 
-    # Gráfica
-    plt.plot(S, valoresY, 'ro', label='puntos')
-    plt.plot(xtraza, ytraza, label='trazador', color='blue')
-    plt.title('Trazadores Cúbicos Naturales')
+    # Grafica
+    grafica(S, valoresY, xtraza, ytraza);
+    return a, b, c, d, Sx
+
+
+#Grafica
+#Entradas:
+            #listaPuntosX: valores que se graficaran en el eje 'x'
+            #listaPuntosY: valores que se graficaran en el eje 'y'
+            #trazaX:
+            #trazay:
+#Salidas:
+            #Grafico con los valores ingresados
+def grafica(listaPuntosX, listaPuntosY, trazaX, trazaY):
+    plt.plot(listaPuntosX, listaPuntosY, 'ro', label = 'puntos')
+    plt.plot(trazaX, trazaY, label = 'trazador', color = 'blue')
+    plt.title('Trazadores Cubicos Naturales')
     plt.xlabel('xi')
     plt.ylabel('S(xi)')
     plt.legend()
     plt.show()
-
-    return a, b, c, d, Sx
-
-def jacobi(A, b, x0, tol):
-    n = len(A)
-    A = np.array(A)
-    b = np.array(b)
-    L = np.zeros((n, n))
-    D = np.zeros((n, n))
-    U = np.zeros((n, n))
-
-    for i in range(n):
-        for j in range(n):
-            if i > j:
-                L[i][j] = A[i][j]
-            elif i < j:
-                U[i][j] = A[i][j]
-            else:
-                D[i][j] = A[i][j]
-
-    x = np.array(x0)
-    error = np.linalg.norm(np.dot(A, x) - b)
-    i = 0
-
-    m_inv = np.linalg.inv(D)
-    m_times_n = np.dot(m_inv, (-1 * (L + U)))
-    m_times_b = np.dot(m_inv, b)
-
-    while error >= tol:
-        x = np.dot(m_times_n, x) + m_times_b
-        error = np.linalg.norm(np.dot(A, x) - b)
-        i += 1
-
-    return x
-
-
-def fact_lu(matrizD, matrizI):
-    if(np.linalg.det(matrizD) == 0):
-        print("La matriz no es singular")
-        return
-    else:
-        pass
-    n = len(matrizD)
-    L = np.eye(n)
-    U = matrizD
-
-    for i in range(1, n):
-        pivot = U[i - 1][i - 1]
-        pivotRow = U[i - 1]
-        M = np.zeros((1, n - i))
-        m = M.size + 1
-
-        for k in range(1, m):
-            try:
-                M[i - 1][k - 1] = (U[i + k - 1][i - 1]) / pivot
-            except:
-                M = (U[i + k - 1][i - 1]) / pivot
-
-        for k in range(1, m):
-            try:
-                U[i + k - 1] = U[i + k - 1] - (np.multiply(pivotRow, M[i - 1][k - 1]))
-                L[i + k - 1][i - 1] = M[i - 1][k - 1]
-            except:
-                U[i + k - 1] = U[i + k - 1] - (np.multiply(pivotRow, M))
-                L[i + k - 1][i - 1] = M
-
-    Y = ((np.linalg.inv(L)).dot(np.transpose(matrizI)))
-    X = (np.linalg.inv(U)).dot(Y)
-    return X
-
-def gauss_seidel(A, b, x0, tol):
-    n = len(A)
-    A = np.array(A)
-    b = np.array(b)
-    L = np.zeros((n, n))
-    D = np.zeros((n, n))
-    U = np.zeros((n, n))
-
-    for i in range(n):
-        for j in range(n):
-            if i > j:
-                L[i][j] = A[i][j]
-            elif i < j:
-                U[i][j] = A[i][j]
-            else:
-                D[i][j] = A[i][j]
-
-    x = np.array(x0)
-    error = np.linalg.norm(np.dot(A, x) - b)
-    i = 0
-
-    m_inv = np.linalg.inv(D + U)
-    m_times_n = np.dot(m_inv, (-1 * L))
-    m_times_b = np.dot(m_inv, b)
-
-    while error >= tol:
-        x = np.dot(m_times_n, x) + m_times_b
-        error = np.linalg.norm(np.dot(A, x) - b)
-        i += 1
-
-    return x
 
 if __name__ == '__main__':
     # Intervalo
