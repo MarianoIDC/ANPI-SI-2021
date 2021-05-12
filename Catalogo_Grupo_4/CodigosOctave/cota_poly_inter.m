0822a17aead2dd565446c504322a765d4a0666f0
MarianoIDC <mmunoz@estudiantec.cr>
2021-05-12 15:19:52 -0600

    Update cota_poly_inter.m

---
+++ Catalogo_Grupo_4/CodigosOctave/cota_poly_inter.m
@@ -0,0 +1,14 @@
+%{
+    Cota de Error del Metodo de la Interpolacion
+    Parametros de Entrada
+        @param f: funcion a la cual se le aplicara el algoritmo
+        @param n: numero de puntos  
+    
+    Parametros de Salida
+        @return xAprox: valor aproximado de x
+        @return error: porcentaje de error del resultado obtenido
+%}
+
+clc
+clear;
+
