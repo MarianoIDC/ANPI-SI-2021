78a54068dc244d6463846b5dd253c9079a045986
Java-Sith <prieto.luisdaniel@gmail.com>
2021-05-12 16:11:12 -0600

    Se resuelve un error de la pregunta 4

--- Tarea 2 - Grupo 4/Parte 2/parte2_p4.py
+++ Tarea 2 - Grupo 4/Parte 2/parte2_p4.py
@@ -6,9 +6,9 @@ from parte2_p2 import *
 x = np.array(['x1','x2','x3','x4'], dtype = object)
 x0 = np.array([1,-1,1,-1], dtype = float)
 f = np.array([
-    '((sin(x2)/(5.08))-((sin(x1))/(3.59))',
-    '((sin(x3)/(6.22))-((sin(x2))/(5.08))',
-    '((sin(x4)/(7.18))-((sin(x3))/(6.22))',
+    '((sin(x2))/(5.08))-((sin(x1))/(3.59))',
+    '((sin(x3))/(6.22))-((sin(x2))/(5.08))',
+    '((sin(x4))/(7.18))-((sin(x3))/(6.22))',
     '(0.2*(tan(x1)+tan(x2)+tan(x3)+tan(x4))-2)'
     ], 
     dtype = object)
