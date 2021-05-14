0966ca7aeb9bb6af1fe4e7ccaca88df4e5b62bbb
MarianoIDC <mmunoz@estudiantec.cr>
2021-05-14 00:04:04 -0600

    Update

--- Catalogo_Grupo_4/CodigosOctave/CotaErrorPolinomioInterpolacion.m
+++ Catalogo_Grupo_4/CodigosOctave/CotaErrorPolinomioInterpolacion.m
@@ -9,7 +9,20 @@
         @return error: porcentaje de error del resultado obtenido
 %}
 
+% |f(x) - p(x)| <= |f^(n+1)(Ex)*1/(n+1!)*(x-x0)(x-x1)...(x-xn)|
+% Ex pertence a [a,b]
+
 clc
 clear;
 
+function [error] = cota_poly_inter(func, vars)
 
+pkg load symbolic
+syms x 
+
+n = length(vars)
+a = 1/factorial(n+1)
+f_aux = diff(func, n+1)
+
+
+end
