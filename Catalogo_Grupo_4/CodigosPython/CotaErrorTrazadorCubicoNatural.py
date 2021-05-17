8edc0780e97d47d675988253ba430e6d92b8dc72
Josue Araya García <jdag98228@gmail.com>
2021-05-16 18:54:12 -0600

    Comentar

--- Catalogo_Grupo_4/CodigosPython/CotaErrorTrazadorCubicoNatural.py
+++ Catalogo_Grupo_4/CodigosPython/CotaErrorTrazadorCubicoNatural.py
@@ -4,25 +4,31 @@ import matplotlib.pyplot as plt
 import sympy
 import sympy as sym
 
+'''
+Cota de error para Trazador Cúbico Natural
+param func: función con al menos 4ta deriva
+param S: Lista de puntos
+return cota_error: Cota de error
+'''
 
 def cota_tras_cubico(func, S):
     h=0
-    x = sym.symbols('x')
-    f = sym.sympify(func)
-    f4 = sym.diff(f,x,4)
+    x = sym.symbols('x')      
+    f = sym.sympify(func)     # La función se castea en x
+    f4 = sym.diff(f,x,4)      # La 4ta derivada
     norm = 0
-    for i in range(len(S)):
-        if (f4.subs(x,S[i])>0):
+    for i in range(len(S)):           # Norma infinira
+        if (f4.subs(x,S[i])>norm):
             norm = f4.subs(x,S[i])
         else:
             pass
     
-    for i in range(len(S)-1):
+    for i in range(len(S)-1):     #Valor h
        if (S[i+1]-S[i]>h):
            h = S[i+1]-S[i]
        else:
            pass
-    cota_error = 5*h**4 * norm /384
+    cota_error = 5*h**4 * norm /384    # Valor cota
     print(float(cota_error))
     return cota_error
 
