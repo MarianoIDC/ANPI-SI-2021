c1cb388420b9d228a0c04ee7206e3fcbf27300db
Jonathana1196 <jonathana1196@gmail.com>
2021-05-26 23:10:07 -0600

    Error Cuadratura Gaussiana

--- Catalogo_Grupo_4/CodigosOctave/CuadraturaGaussiana.m
+++ Catalogo_Grupo_4/CodigosOctave/CuadraturaGaussiana.m
@@ -21,20 +21,22 @@ warning("off","all");
 % pkg install -forge miscellaneous
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [xAprox] = cuad_gaussiana(funcion, a, b, n)
+function [xAprox, err] = cuad_gaussiana(funcion, a, b, n)
     xAprox = 0;
     i = 1;
     legendrePol = legendrepoly(n);
     cerosPol = roots(legendrePol);
     derPol = polyder(legendrePol);
     while(i <= n)
-        xi = cerosPol(i)
-        Pi = polyval(derPol, xi)
-        wi = 2/((1-(xi^2))*(Pi)^2)
+        xi = cerosPol(i);
+        Pi = polyval(derPol, xi);
+        wi = 2/((1-(xi^2))*(Pi)^2);
         xAprox = xAprox + wi*funcion(xi);
         i = i + 1;
     endwhile
-    xAprox
+    q = integral(funcion, a, b);
+    xAprox;
+    err = abs(xAprox - q);
 endfunction
 
 %Intervalos de la integral
@@ -43,9 +45,9 @@ b = 1;
 %Grado del polinomio
 n = 3;
 %Funcion 
-funct = @(x) (e^x)*cos(x);
+funct = @(x) exp(x).*cos(x);
 %Llamado de la funcion
-[xAprox] =  cuad_gaussiana(funct, a, b, n);
+[xAprox, err] =  cuad_gaussiana(funct, a, b, n);
 printf("############################################ \n");
 printf("Metodo de las Cuadraturas Gaussianas \n");
-%printf('xAprox = %f\n%%Error = %d \n', xAprox, err);
+printf('xAprox = %f\n%%Error = %d \n', xAprox, err);
